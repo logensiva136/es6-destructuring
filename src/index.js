68dc3fbd3b3e638ac90785ad2e0732549865e625
Logentheran <53439230+logensiva136@users.noreply.github.com>
2021-06-17 15:12:21 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  ,
+  document.getElementById("root")
+);
